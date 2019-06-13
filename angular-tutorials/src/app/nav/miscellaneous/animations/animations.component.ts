40e1386ecbb77b855d079f5dec5207e8060b1025
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-06-13 12:15:07 +0530

    Angular 7 - Animations

--- angular-tutorials/src/app/nav/miscellaneous/animations/animations.component.ts
+++ angular-tutorials/src/app/nav/miscellaneous/animations/animations.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-animations',
   templateUrl: './animations.component.html',
-  styleUrls: ['./animations.component.scss']
+  styleUrls: ['./animations.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          height: '200px',
+          opacity: 1,
+          backgroundColor: 'yellow'
+        })
+      ),
+      state(
+        'closed',
+        style({
+          height: '100px',
+          opacity: 0.5,
+          backgroundColor: 'green'
+        })
+      ),
+      transition('open => closed', [animate('1s')]),
+      transition('closed => open', [animate('0.5s')])
+    ])
+  ]
 })
-export class AnimationsComponent implements OnInit {
+export class AnimationsComponent {
+ 
+  isOpen = true;
 
-  constructor() { }
-
-  ngOnInit() {
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
-
 }
