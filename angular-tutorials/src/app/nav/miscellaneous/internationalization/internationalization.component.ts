7d21475cd266280386c11bb3fb45b3a0e1d7c07b
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-06-16 22:46:13 +0530

    Angular 7 - Internationalization

--- angular-tutorials/src/app/nav/miscellaneous/internationalization/internationalization.component.ts
+++ angular-tutorials/src/app/nav/miscellaneous/internationalization/internationalization.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './internationalization.component.html',
   styleUrls: ['./internationalization.component.scss']
 })
-export class InternationalizationComponent implements OnInit {
+export class InternationalizationComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  minutes = 0;
+  gender = 'female';
+  fly = true;
+  logo = 'https://angular.io/assets/images/logos/angular/angular.png';
+  heroes: string[] = ['Magneta', 'Celeritas', 'Dynama'];
+  inc(i: number) {
+    this.minutes = Math.min(5, Math.max(0, this.minutes + i));
   }
+  male() { this.gender = 'male'; }
+  female() { this.gender = 'female'; }
+  other() { this.gender = 'other'; }
 
 }
