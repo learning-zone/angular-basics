367fd7d77e9198cc9c5eadb1b7f45d942c5b58e8
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-08-02 10:35:54 +0530

    Lifecycle Sequence

--- angular-features/src/app/nav/component/directives/structural-directives.directive.ts
+++ angular-features/src/app/nav/component/directives/structural-directives.directive.ts
@@ -3,8 +3,4 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: '[appStructuralDirectives]'
 })
-export class StructuralDirectivesDirective {
-
-  constructor() { }
-
-}
+export class StructuralDirectivesDirective { }
