d62ea6fa2a6ad1d2cc260eebf2ed3542e6da9043
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-08-20 12:25:07 +0530

    Update app.component.spec.ts

--- angular-features/src/app/app.component.spec.ts
+++ angular-features/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-pro-sidebar'`, () => {
+  it(`should have as title 'angular-features'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-pro-sidebar');
+    expect(app.title).toEqual('angular-features');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-pro-sidebar!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-features!');
   });
 });
