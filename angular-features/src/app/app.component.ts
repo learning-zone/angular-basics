06ecb88add3c56b6961ba6dc41fc9b7d0759b8cd
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-08-25 12:13:23 +0530

    Web Wokers

--- angular-features/src/app/app.component.ts
+++ angular-features/src/app/app.component.ts
@@ -23,3 +23,15 @@ export class AppComponent {
     this.sidebarservice.setSidebarState(true);
   }
 }
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker('./app.worker', { type: 'module' });
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
