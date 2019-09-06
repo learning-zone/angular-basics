49d12f6999867c0fed4ce27145e00c2e249b3909
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-06 23:29:49 +0530

    fix: ng lint error

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
 
 
 @NgModule({
