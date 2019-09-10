14e8932b930cf195ba0abb0223d0eeb3fa7d1d24
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-10 09:09:30 +0530

    Routing

--- angular-material/src/app/app-routing.module.ts
+++ angular-material/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { AutocompleteComponent } from '../app/components/pages/autocomplete/autocomplete.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'autocomplete', component: AutocompleteComponent },
+
+
+  { path: 'home', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
