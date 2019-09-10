4b8d7a9f66b84e6d609a3ce4f896b844fd00f245
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-10 11:37:30 +0530

    Dynamically set the page title based on the active route

--- angular-material/src/app/app.component.ts
+++ angular-material/src/app/app.component.ts
@@ -21,7 +21,6 @@ export class AppComponent implements OnInit {
     this.menus = sidebarservice.getMenuList();
   }
 
-
   ngOnInit() {
     const appTitle = this.titleService.getTitle();
     this.router
