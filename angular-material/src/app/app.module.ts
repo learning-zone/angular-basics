10c07f2813c0c07e9fe5d19fa08f55851913430a
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-10 23:01:30 +0530

    Material Module

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatInputModule } from '@angular/material/input';
 import { HttpClientModule } from '@angular/common/http';
-import { MatExpansionModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
+
+import { AppMaterialModule } from './app.material.module';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AutocompleteComponent } from './components/pages/autocomplete/autocomplete.component';
 import { CheckboxComponent } from './components/pages/checkbox/checkbox.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 
 
 @NgModule({
@@ -28,16 +24,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatButtonModule,
-    MatIconModule,
-    MatExpansionModule,
-    MatAutocompleteModule,
-    MatInputModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppMaterialModule
   ],
   providers: [Title],
   bootstrap: [AppComponent]
