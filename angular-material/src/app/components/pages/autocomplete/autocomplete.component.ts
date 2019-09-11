4f7859db7f731501060ad7654183c0029e48f0d5
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-11 11:33:28 +0530

    Filter Autocomplete

--- angular-material/src/app/components/pages/autocomplete/autocomplete.component.ts
+++ angular-material/src/app/components/pages/autocomplete/autocomplete.component.ts
@@ -1,21 +1,29 @@
-import {Component} from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-
-export interface State {
-  flag: string;
-  name: string;
-  population: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-autocomplete',
   templateUrl: './autocomplete.component.html',
   styleUrls: ['./autocomplete.component.scss']
 })
-export class AutocompleteComponent {
+export class AutocompleteComponent implements OnInit {
   myControl = new FormControl();
   options: string[] = ['RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'INDIGO', 'VIOLET'];
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
