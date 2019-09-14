83a9d74ecc90f0dfa0f673e61ad23258584094df
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-14 18:22:46 +0530

    Tooltip with a custom position

--- angular-material/src/app/components/pages/tooltip/tooltip.component.ts
+++ angular-material/src/app/components/pages/tooltip/tooltip.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { TooltipPosition } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-tooltip',
   templateUrl: './tooltip.component.html',
   styleUrls: ['./tooltip.component.scss']
 })
-export class TooltipComponent implements OnInit {
+export class TooltipComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  positionOptions: TooltipPosition[] = ['after', 'before', 'above', 'below', 'left', 'right'];
+  position = new FormControl(this.positionOptions[0]);
 
 }
