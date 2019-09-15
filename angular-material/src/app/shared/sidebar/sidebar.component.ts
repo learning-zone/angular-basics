3af3a4c053aa27331e658ed7effa7bbb7d0ae066
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 15:46:25 +0530

    Sidebar Links

---
+++ angular-material/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { SidebarService } from './sidebar.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+  menus = [];
+  days = [];
+  constructor(public sidebarservice: SidebarService
+    ) { 
+    this.menus = sidebarservice.menus;
+    this.days = sidebarservice.days;
+  }
+ 
+  ngOnInit() {
+  }
+
+}
