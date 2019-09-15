eb7bb8dc852bbe9675ca5bef05afd6bf2efbdb51
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 23:03:01 +0530

    Drag & Drop custom placeholer

--- angular-material/src/app/cdk/pages/drag-drop/custom-placeholer/custom-placeholer.component.ts
+++ angular-material/src/app/cdk/pages/drag-drop/custom-placeholer/custom-placeholer.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-custom-placeholer',
   templateUrl: './custom-placeholer.component.html',
   styleUrls: ['./custom-placeholer.component.scss']
 })
-export class CustomPlaceholerComponent implements OnInit {
+export class CustomPlaceholerComponent {
 
-  constructor() { }
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi'
+  ];
 
-  ngOnInit() {
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
   }
-
 }
