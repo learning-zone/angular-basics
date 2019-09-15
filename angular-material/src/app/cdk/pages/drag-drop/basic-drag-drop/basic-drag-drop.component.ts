3f3e62302c2d15022f138b01088b201cb8f5f1fa
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 22:02:00 +0530

    Basic Drag & Drop

---
+++ angular-material/src/app/cdk/pages/drag-drop/basic-drag-drop/basic-drag-drop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-drag-drop',
+  templateUrl: './basic-drag-drop.component.html',
+  styleUrls: ['./basic-drag-drop.component.scss']
+})
+export class BasicDragDropComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
