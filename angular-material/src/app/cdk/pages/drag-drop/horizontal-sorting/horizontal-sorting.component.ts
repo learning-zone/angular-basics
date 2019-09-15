cdbb22ef32a6c9105b6db076a7d71b9839284fef
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 22:04:05 +0530

    Horizontal Sorting

---
+++ angular-material/src/app/cdk/pages/drag-drop/horizontal-sorting/horizontal-sorting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-sorting',
+  templateUrl: './horizontal-sorting.component.html',
+  styleUrls: ['./horizontal-sorting.component.scss']
+})
+export class HorizontalSortingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
