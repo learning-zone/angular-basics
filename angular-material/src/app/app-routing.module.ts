f3be0fc9ec243b73c730f96b7c8031aaf418619f
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 15:55:26 +0530

    Material Component Routing

--- angular-material/src/app/app-routing.module.ts
+++ angular-material/src/app/app-routing.module.ts
@@ -27,27 +27,24 @@ const routes: Routes = [
   { path: 'home', redirectTo: '/', pathMatch: 'full', data: {title: 'Home | Angular Material'}},
   // { path: '**', component: PageNotFoundComponent, data: {title: 'Page Not Found | Angular Material'}}
 
-  { path: 'autocomplete', component: AutocompleteComponent, data: {title: 'Autocomplete | Angular Material'}},
-  { path: 'checkbox', component: CheckboxComponent, data: {title: 'Checkbox | Angular Material'}},
-  { path: 'datepicker', component: DatepickerComponent, data: {title: 'Datepicker | Angular Material'}},
-  { path: 'material-form', component: FormComponent, data: {title: 'Form | Angular Material'}},
-  { path: 'paginator', component: PaginatorComponent, data: {title: 'Paginator | Angular Material'}},
-  { path: 'sort-header', component: SortHeaderComponent, data: {title: 'SortHeader | Angular Material'}},
-  { path: 'card', component: CardComponent, data: {title: 'Card | Angular Material'}},
-  { path: 'tabs', component: TabsComponent, data: {title: 'Tabs | Angular Material'}},
-  { path: 'list', component: ListComponent, data: {title: 'List | Angular Material'}},
-
-  { path: 'button', component: ButtonComponent, data: {title: 'Button | Angular Material'}},
-  { path: 'badge', component: BadgeComponent, data: {title: 'Badge | Angular Material'}},
-  { path: 'chips', component: ChipsComponent, data: {title: 'Chips | Angular Material'}},
-  { path: 'progress-spinner', component: ProgressSpinnerComponent, data: {title: 'ProgressSpinner | Angular Material'}},
-  { path: 'progress-bar', component: ProgressBarComponent, data: {title: 'ProgressBar | Angular Material'}},
-
-  { path: 'bottom-sheet', component: BottomSheetComponent, data: {title: 'BottomSheet | Angular Material'}},
-  { path: 'dialog', component: DialogComponent, data: {title: 'Dialog | Angular Material'}},
-  { path: 'snackbar', component: SnackbarComponent, data: {title: 'Snackbar | Angular Material'}},
-  { path: 'tooltip', component: TooltipComponent, data: {title: 'Tooltip | Angular Material'}},
-
+  { path: 'components/autocomplete', component: AutocompleteComponent, data: {title: 'Autocomplete | Angular Material'}},
+  { path: 'components/checkbox', component: CheckboxComponent, data: {title: 'Checkbox | Angular Material'}},
+  { path: 'components/datepicker', component: DatepickerComponent, data: {title: 'Datepicker | Angular Material'}},
+  { path: 'components/material-form', component: FormComponent, data: {title: 'Form | Angular Material'}},
+  { path: 'components/paginator', component: PaginatorComponent, data: {title: 'Paginator | Angular Material'}},
+  { path: 'components/sort-header', component: SortHeaderComponent, data: {title: 'SortHeader | Angular Material'}},
+  { path: 'components/card', component: CardComponent, data: {title: 'Card | Angular Material'}},
+  { path: 'components/tabs', component: TabsComponent, data: {title: 'Tabs | Angular Material'}},
+  { path: 'components/list', component: ListComponent, data: {title: 'List | Angular Material'}},
+  { path: 'components/button', component: ButtonComponent, data: {title: 'Button | Angular Material'}},
+  { path: 'components/badge', component: BadgeComponent, data: {title: 'Badge | Angular Material'}},
+  { path: 'components/chips', component: ChipsComponent, data: {title: 'Chips | Angular Material'}},
+  { path: 'components/progress-spinner', component: ProgressSpinnerComponent, data: {title: 'ProgressSpinner | Angular Material'}},
+  { path: 'components/progress-bar', component: ProgressBarComponent, data: {title: 'ProgressBar | Angular Material'}},
+  { path: 'components/bottom-sheet', component: BottomSheetComponent, data: {title: 'BottomSheet | Angular Material'}},
+  { path: 'components/dialog', component: DialogComponent, data: {title: 'Dialog | Angular Material'}},
+  { path: 'components/snackbar', component: SnackbarComponent, data: {title: 'Snackbar | Angular Material'}},
+  { path: 'components/tooltip', component: TooltipComponent, data: {title: 'Tooltip | Angular Material'}}
 
 ];
 
