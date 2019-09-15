8632eb0d1badbbefa423fb7d070d010bfc0ebc82
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 16:12:50 +0530

    Sidebar Component

--- angular-material/src/app/shared/sidebar/sidebar.component.ts
+++ angular-material/src/app/shared/sidebar/sidebar.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SidebarService } from './sidebar.service';
+
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
   menus = [];
   days = [];
-  constructor(public sidebarservice: SidebarService
-    ) { 
+  constructor(
+    public sidebarservice: SidebarService
+    ) {
     this.menus = sidebarservice.menus;
     this.days = sidebarservice.days;
-  }
- 
-  ngOnInit() {
   }
 
 }
