54869a35b3fd01e1e15b0b6b47bb57f1b7568a25
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 23:02:24 +0530

    Drag & Drop connected sorting

--- angular-material/src/app/cdk/pages/drag-drop/connected-sorting/connected-sorting.component.ts
+++ angular-material/src/app/cdk/pages/drag-drop/connected-sorting/connected-sorting.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-connected-sorting',
   templateUrl: './connected-sorting.component.html',
   styleUrls: ['./connected-sorting.component.scss']
 })
-export class ConnectedSortingComponent implements OnInit {
+export class ConnectedSortingComponent {
 
-  constructor() { }
+  todo = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ];
 
-  ngOnInit() {
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
   }
-
 }
