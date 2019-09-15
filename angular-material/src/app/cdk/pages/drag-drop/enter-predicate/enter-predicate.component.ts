a8228841c10d3a4041d13efc36812c99abb50bc0
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 22:03:48 +0530

    Enter Predicate

---
+++ angular-material/src/app/cdk/pages/drag-drop/enter-predicate/enter-predicate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-predicate',
+  templateUrl: './enter-predicate.component.html',
+  styleUrls: ['./enter-predicate.component.scss']
+})
+export class EnterPredicateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
