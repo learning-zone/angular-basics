69561e8f78da23f9df37bdd07835172ff49c9d5b
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 22:03:03 +0530

    Custom Placeholer

---
+++ angular-material/src/app/cdk/pages/drag-drop/custom-placeholer/custom-placeholer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-placeholer',
+  templateUrl: './custom-placeholer.component.html',
+  styleUrls: ['./custom-placeholer.component.scss']
+})
+export class CustomPlaceholerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
