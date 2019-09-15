51c8d743360d2c5e5e20f96f22a6221d3b7e6250
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 22:02:43 +0530

    Connected Sorting

---
+++ angular-material/src/app/cdk/pages/drag-drop/connected-sorting/connected-sorting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connected-sorting',
+  templateUrl: './connected-sorting.component.html',
+  styleUrls: ['./connected-sorting.component.scss']
+})
+export class ConnectedSortingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
