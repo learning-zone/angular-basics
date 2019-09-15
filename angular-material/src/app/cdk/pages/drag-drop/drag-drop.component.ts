5707e0755a32dff10e5117db93f13a86c99bb72e
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 22:11:04 +0530

    Update drag-drop.component.ts

--- angular-material/src/app/cdk/pages/drag-drop/drag-drop.component.ts
+++ angular-material/src/app/cdk/pages/drag-drop/drag-drop.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-drag-drop',
-  templateUrl: './drag-drop.component.html',
-  styleUrls: ['./drag-drop.component.scss']
+  template: `
+  <h3>Drag & Drop<hr/></h3>
+    <mat-tab-group>
+      <mat-tab label="Basic Drag & Drop"> <app-basic-drag-drop></app-basic-drag-drop> </mat-tab>
+      <mat-tab label="Sorting"> <app-sorting></app-sorting> </mat-tab>
+      <mat-tab label="Connected Sorting"> <app-connected-sorting></app-connected-sorting> </mat-tab>
+      <mat-tab label="With a handle"> <app-with-a-handle></app-with-a-handle> </mat-tab>
+      <mat-tab label="Custom Preview"> <app-custom-preview></app-custom-preview> </mat-tab>
+      <mat-tab label="Custom Placeholer"> <app-custom-placeholer></app-custom-placeholer> </mat-tab>
+      <mat-tab label="Horizontal Sorting"> <app-horizontal-sorting></app-horizontal-sorting> </mat-tab>
+      <mat-tab label="Boundary"> <app-boundary></app-boundary> </mat-tab>
+      <mat-tab label="Position Locking"> <app-position-locking></app-position-locking> </mat-tab>
+      <mat-tab label="With alternate root element"> <app-with-alternate-root-element></app-with-alternate-root-element> </mat-tab>
+      <mat-tab label="Enter Predicate"> <app-enter-predicate></app-enter-predicate> </mat-tab>
+    </mat-tab-group>
+  `
 })
-export class DragDropComponent implements OnInit {
+export class DragDropComponent { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
