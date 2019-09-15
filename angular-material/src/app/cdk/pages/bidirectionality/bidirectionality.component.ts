c4517c39c141cf61b72eb84c530daae85f067f24
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 16:16:10 +0530

    Bidirectionality Component

---
+++ angular-material/src/app/cdk/pages/bidirectionality/bidirectionality.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bidirectionality',
+  templateUrl: './bidirectionality.component.html',
+  styleUrls: ['./bidirectionality.component.scss']
+})
+export class BidirectionalityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
