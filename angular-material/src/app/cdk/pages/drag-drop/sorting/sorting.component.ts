effce4041a248f1aab1b7debe533751859b0b9fd
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 23:05:08 +0530

    Drag & Drop Sorting

--- angular-material/src/app/cdk/pages/drag-drop/sorting/sorting.component.ts
+++ angular-material/src/app/cdk/pages/drag-drop/sorting/sorting.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-sorting',
   templateUrl: './sorting.component.html',
   styleUrls: ['./sorting.component.scss']
 })
-export class SortingComponent implements OnInit {
+export class SortingComponent {
 
-  constructor() { }
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi'
+  ];
 
-  ngOnInit() {
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
   }
 
 }
