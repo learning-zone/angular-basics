0c90ada486cf02bf36bb6f5e1efef2d0a0cd7a30
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 16:18:52 +0530

    Text field Component

---
+++ angular-material/src/app/cdk/pages/text-field/text-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-field',
+  templateUrl: './text-field.component.html',
+  styleUrls: ['./text-field.component.scss']
+})
+export class TextFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
