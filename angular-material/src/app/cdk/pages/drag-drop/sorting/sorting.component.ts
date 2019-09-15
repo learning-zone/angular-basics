af8acc884e6f015b8db064d2f0c6056761abebd5
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 22:04:38 +0530

    Sorting

---
+++ angular-material/src/app/cdk/pages/drag-drop/sorting/sorting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sorting',
+  templateUrl: './sorting.component.html',
+  styleUrls: ['./sorting.component.scss']
+})
+export class SortingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
