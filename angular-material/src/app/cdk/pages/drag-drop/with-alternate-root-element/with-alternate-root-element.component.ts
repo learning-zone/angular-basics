58a28f38ed74075bed14bf8f091385cfb8611bc3
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 22:05:22 +0530

    With alternate root element

---
+++ angular-material/src/app/cdk/pages/drag-drop/with-alternate-root-element/with-alternate-root-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-with-alternate-root-element',
+  templateUrl: './with-alternate-root-element.component.html',
+  styleUrls: ['./with-alternate-root-element.component.scss']
+})
+export class WithAlternateRootElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
