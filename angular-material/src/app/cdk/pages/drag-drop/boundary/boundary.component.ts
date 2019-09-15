1c98e682944a05a0b2606d826a547e7fca831772
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 22:02:22 +0530

    Boundary

---
+++ angular-material/src/app/cdk/pages/drag-drop/boundary/boundary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boundary',
+  templateUrl: './boundary.component.html',
+  styleUrls: ['./boundary.component.scss']
+})
+export class BoundaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
