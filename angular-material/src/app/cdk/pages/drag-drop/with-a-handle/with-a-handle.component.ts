8c5650b24eec09a95afb8f18152fbad2a2b0e4ad
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 22:05:01 +0530

    With a handle

---
+++ angular-material/src/app/cdk/pages/drag-drop/with-a-handle/with-a-handle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-with-a-handle',
+  templateUrl: './with-a-handle.component.html',
+  styleUrls: ['./with-a-handle.component.scss']
+})
+export class WithAHandleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
