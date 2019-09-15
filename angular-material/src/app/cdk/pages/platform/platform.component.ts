274165fc4043463fb2c13d7f8757a6d568bcba2d
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 16:17:53 +0530

    Platform Component

---
+++ angular-material/src/app/cdk/pages/platform/platform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-platform',
+  templateUrl: './platform.component.html',
+  styleUrls: ['./platform.component.scss']
+})
+export class PlatformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
