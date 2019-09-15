ff56b56e4a7397912c6b177b2d2457302c0978bd
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-15 16:17:32 +0530

    Overlay Component

---
+++ angular-material/src/app/cdk/pages/overlay/overlay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overlay',
+  templateUrl: './overlay.component.html',
+  styleUrls: ['./overlay.component.scss']
+})
+export class OverlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
