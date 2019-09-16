fceaf8e41e50991d8a1135ffc3a9676e0f82cba4
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-16 10:29:40 +0530

    Monitoring autofill state with AutofillMonitor

--- angular-material/src/app/cdk/pages/text-field/text-field.component.ts
+++ angular-material/src/app/cdk/pages/text-field/text-field.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {AutofillMonitor} from '@angular/cdk/text-field';
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-text-field',
   templateUrl: './text-field.component.html',
   styleUrls: ['./text-field.component.scss']
 })
-export class TextFieldComponent implements OnInit {
+export class TextFieldComponent implements AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('first', { read: ElementRef, static: false }) firstName: ElementRef<HTMLElement>;
+  @ViewChild('last', { read: ElementRef, static: false }) lastName: ElementRef<HTMLElement>;
+  firstNameAutofilled: boolean;
+  lastNameAutofilled: boolean;
 
-  ngOnInit() {
+  constructor(private _autofill: AutofillMonitor) {}
+
+  ngAfterViewInit() {
+    this._autofill.monitor(this.firstName)
+        .subscribe(e => this.firstNameAutofilled = e.isAutofilled);
+    this._autofill.monitor(this.lastName)
+        .subscribe(e => this.lastNameAutofilled = e.isAutofilled);
   }
 
+  ngOnDestroy() {
+    this._autofill.stopMonitoring(this.firstName);
+    this._autofill.stopMonitoring(this.lastName);
+  }
 }
