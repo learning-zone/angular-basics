df431a08d06541645b926e71a1e61e3f1e5dc46c
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-12-17 11:22:11 +0530

    Update component-interaction.component.ts

--- angular-features/src/app/nav/component/component-interaction/component-interaction.component.ts
+++ angular-features/src/app/nav/component/component-interaction/component-interaction.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-component-interaction',
   templateUrl: './component-interaction.component.html',
   styleUrls: ['./component-interaction.component.scss']
 })
-export class ComponentInteractionComponent implements OnInit {
+export class ComponentInteractionComponent {
   title: string;
   output: string;
   numbers: Array<number>;
@@ -14,8 +14,6 @@ export class ComponentInteractionComponent implements OnInit {
     this.title = 'Number Array';
     this.numbers = [10, 20, 30, 40, 50];
   }
-
-  ngOnInit() {}
 
   call(value: string): void {
     this.output = '[ You Clicked: ' + value + ' ]';
