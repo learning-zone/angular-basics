977f21eb23d50d9e0dd38fdfc5f057a0d3b0d910
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-01-31 07:03:25 +0530

    Routing

--- angular-features/src/app/app.module.ts
+++ angular-features/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ScrollingModule } from '@angular/cdk/scrolling';
