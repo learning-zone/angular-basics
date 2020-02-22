5f0bda110a0cc06ab8d58871c5b934242be3c6a5
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-02-22 22:35:21 +0530

    Web-Worker

--- performance-tuning/src/app/app.component.ts
+++ performance-tuning/src/app/app.component.ts
@@ -8,3 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'performance-tuning';
 }
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker('./app.worker', { type: 'module' });
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
