45ae6e1bf39f85083e9f1386ff0f4e2b30f2dfa0
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-03-16 22:57:58 +0530

    TrackBy

--- performance-tuning/src/app/components/runtime-optimizations/trackby/trackby.component.ts
+++ performance-tuning/src/app/components/runtime-optimizations/trackby/trackby.component.ts
@@ -5,11 +5,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './trackby.component.html',
   styleUrls: ['./trackby.component.scss']
 })
-export class TrackbyComponent implements OnInit {
+export class TrackbyComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  collection: { id: number; }[];
+  noOfClick = 0;
+  constructor() {
+    this.collection = [{id: 1}, {id: 2}, {id: 3}];
   }
 
+  getItems() {
+    this.noOfClick++;
+    this.collection = this.getItemsFromServer();
+  }
+
+  getItemsFromServer() {
+    return [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}];
+  }
+
+  trackByFn(index: any, item: any) {
+    return index;
+  }
 }
